a5000b1b2b55d575a3d664ad138dfa9fe4541895
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2025-06-12 14:55:15 +0200

    feat(nvim): copilot

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,7 +60,8 @@ require("lazy").setup({
 		{ 'nvim-tree/nvim-tree.lua' },
 		{ 'nvim-tree/nvim-web-devicons' },
 		{ 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
-		{ 'lewis6991/gitsigns.nvim' }
+		{ 'lewis6991/gitsigns.nvim' },
+		{ 'zbirenbaum/copilot.lua' }
 	},
 	-- automatically check for plugin updates
 	checker = { enabled = true },
@@ -286,3 +287,59 @@ require('gitsigns').setup {
     col = 1
   },
 }
+
+require("copilot").setup({
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "bottom", -- | top | left | right | horizontal | vertical
+      ratio = 0.2
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    hide_during_completion = true,
+    debounce = 75,
+    trigger_on_accept = true,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  should_attach = function(_, bufname)
+	if not string.find(bufname, "github.com") then
+	  return false
+	end
+    if not vim.bo.buflisted then
+      return false
+    end
+    if vim.bo.buftype ~= "" then
+      return false
+    end
+    return true
+  end,
+})
